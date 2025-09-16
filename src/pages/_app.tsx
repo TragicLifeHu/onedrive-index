95a17c7d663a50f2618468c119521e304ba46064
Nyan Fantasia <nyanfantasia@gmail.com>
2025-09-17 03:24:51 +0800

    Refactor imports and clean up CSS styles for improved readability

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import '../styles/markdown-github.css'
 
 // Require had to be used to prevent SSR failure in Next.js
 // Related discussion: https://github.com/FortAwesome/Font-Awesome/issues/19348
-const { library, config } = require('@fortawesome/fontawesome-svg-core')
+import { library, config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
 import {
