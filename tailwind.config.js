fa02790446136e3c4cbe4214af47eb9802ae6fe0
Nyan Fantasia <nyanfantasia@gmail.com>
2025-09-17 03:39:11 +0800

    Update Tailwind CSS configuration to include CSS files in content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors')
 const siteConfig = require('./config/site.config')
 
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,css}'],
   theme: {
     colors: {
       transparent: 'transparent',
