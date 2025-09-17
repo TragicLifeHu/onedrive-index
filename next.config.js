fadc79dfe97299c82527fb7780e6889336a5069d
Nyan Fantasia <nyanfantasia@gmail.com>
2025-09-17 15:09:25 +0800

    Update configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-const path = require('path')
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   reactStrictMode: true,
+
   // Transpile ESM packages for proper module resolution
   transpilePackages: ['plyr', 'plyr-react', 'react-doc-viewer'],
+
+  // output: 'export',
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
+
+module.exports = nextConfig
