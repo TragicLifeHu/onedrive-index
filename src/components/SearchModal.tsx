a2353bc2ec1f17904bca5022b1008b367f22f770
Nyan Fantasia <nyanfantasia@gmail.com>
2025-09-18 07:20:45 +0800

    Fix search result

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -19,27 +19,20 @@ import siteConfig from '../../config/site.config'
  * absolute path
  *
  * @param path Path returned from the parentReference field of the driveItem
- * @param fullPath Whether to return the path including the baseDirectory
  * @returns The absolute path of the driveItem in the search result
  */
-function mapAbsolutePath(path: string, fullPath = true): string {
+function mapAbsolutePath(path: string): string {
   // path is in the format of '/drive/root:/path/to/file', if baseDirectory is '/' then we split on 'root:',
   // otherwise we split on the user defined 'baseDirectory'
   const absolutePath = path.split(siteConfig.baseDirectory === '/' ? 'root:' : siteConfig.baseDirectory)
   // path returned by the API may contain #, by doing a decodeURIComponent and then encodeURIComponent we can
   // replace URL sensitive characters such as the # with %23
-  const finalPath =
-    absolutePath.length > 1 // solve https://github.com/spencerwooo/onedrive-vercel-index/issues/539
-      ? absolutePath[1]
-          .split('/')
-          .map(p => encodeURIComponent(decodeURIComponent(p)))
-          .join('/')
-      : ''
-  const cutOutPath =
-    siteConfig.baseDirectory === '/' ?
-      finalPath.replace('/root:', '') :
-      finalPath.replace(siteConfig.baseDirectory, '')
-  return fullPath ? finalPath : cutOutPath
+  return absolutePath.length > 1 // solve https://github.com/spencerwooo/onedrive-vercel-index/issues/539
+    ? absolutePath[1]
+        .split('/')
+        .map(p => encodeURIComponent(decodeURIComponent(p)))
+        .join('/')
+    : ''
 }
 
 /**
@@ -138,7 +131,8 @@ function SearchResultItemLoadRemote({ result }: { result: OdSearchResult[number]
     )
   }
 
-  const driveItemPath = `${mapAbsolutePath(data.parentReference.path, false)}/${encodeURIComponent(data.name)}`
+  const path = `/${encodeURIComponent(data.name)}`.replace(siteConfig.baseDirectory, '')
+  const driveItemPath = `${mapAbsolutePath(data.parentReference.path)}` + path
   return (
     <SearchResultItemTemplate
       driveItem={result}
