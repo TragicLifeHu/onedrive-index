00c99426f922173e4b45fc65ebaee5ed32911978
Nyan Fantasia <nyanfantasia@gmail.com>
2025-09-18 00:21:48 +0800

    Separate health-check function

--- src/utils/odAuthTokenStore.ts
+++ src/utils/odAuthTokenStore.ts
@@ -1,13 +1,21 @@
 // Add import for Cloudflare context
 import { getCfEnv } from './cfContext';
 
-export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
-  // Get Cloudflare environment from AsyncLocalStorage
+// Add helper to get the OneDrive KV namespace
+function getOdKv() {
   const env = getCfEnv();
   if (!env) {
-    throw new Error('Cloudflare environment not available. Make sure setCfEnv is called in the worker.')
+    throw new Error('Cloudflare environment not available. Make sure setCfEnv is called in the worker.');
   }
   const kv = env.ONEDRIVE_CF_INDEX_KV;
+  if (!kv) {
+    throw new Error('ONEDRIVE_CF_INDEX_KV binding not found.');
+  }
+  return kv;
+}
+
+export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
+  const kv = getOdKv();
   const accessToken = await kv.get('access_token');
   const refreshToken = await kv.get('refresh_token');
 
@@ -26,12 +34,7 @@ export async function storeOdAuthTokens({
   accessTokenExpiry: number
   refreshToken: string
 }): Promise<void> {
-  const env = getCfEnv()
-  if (!env) {
-    throw new Error('Cloudflare environment not available. Make sure setCfEnv is called in the worker.')
-  }
-
-  const kv = env.ONEDRIVE_CF_INDEX_KV
+  const kv = getOdKv();
   await kv.put('access_token', accessToken, { expirationTtl: accessTokenExpiry })
   await kv.put('refresh_token', refreshToken)
 }
